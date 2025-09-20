e0642fe9865c585fdd20bdaf6c84612f216f65f4
giupe <giupe@MSI>
2025-09-20 22:53:21 +0200

    Add project files.

---
+++ 19.22/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _19._22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numreri = new List<int>();
+            Random random = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                numreri.Add(random.Next(1, 101));
+            }
+            foreach (int n in numreri)
+            {
+                Console.WriteLine(n);
+            }
+            Console.WriteLine("--------------");
+            numreri.Reverse();
+            for (int i = 0; i < numreri.Count; i++)
+            {
+                Console.WriteLine(numreri[i]);
+            }
+        }
+    }
+}
